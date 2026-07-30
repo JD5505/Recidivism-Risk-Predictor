711a2d004f1b835aaf6aa68341f0f6e8a96a94f6
Your Name <you@example.com>
2026-07-30 12:09:07 +0530

    added live backend link

--- frontend/index.py
+++ frontend/index.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = 'http://13.235.103.164:8000/predict'
+API_URL = 'https://recidivism-risk-predictor.onrender.com/predict'
 
 st.set_page_config(
     page_title="Recidivism Risk Predictor",
